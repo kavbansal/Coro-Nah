d6cbc59392f6b2617591ae200c94475051f86794
TBT <tahseenbintaj@hotmail.com>
2020-09-12 13:23:26 -0400

    Update app.py
    Changed the template folder to the current folder and turned debug mode on.

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask
 from flask import render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder=".")
 
 @app.route("/")
 def index():
 	return render_template("index.html")
 
 if __name__ == "__main__":
-	app.run()
+	app.run(debug=True)
